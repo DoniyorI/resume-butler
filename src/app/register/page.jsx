f04f4f2126df535deadb4acd13534add3f542711
DoniyorI <doniyor.ismatilloev@gmail.com>
2026-03-15 12:17:48 -0400

    fix: dynamic import carousel to fix SSR build error

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -8,7 +8,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-import EmblaCarousel from "@/components/Carousel";
+import dynamic from "next/dynamic";
+const EmblaCarousel = dynamic(() => import("@/components/Carousel"), { ssr: false });
 
 import { createClient } from "@/lib/supabase/client";
 import GoogleAuthButton from "@/components/GoogleAuth";
